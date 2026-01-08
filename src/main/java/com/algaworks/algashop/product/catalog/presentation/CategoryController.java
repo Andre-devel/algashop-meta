cb8a75b9055b7b7639737810d24ecc836750bc29
Andre goncalves <andrevilanovagon@hotmail.com>
2026-01-07 21:45:34 -0300

    feat: add initial product and category management functionality with API endpoints

---
+++ src/main/java/com/algaworks/algashop/product/catalog/presentation/CategoryController.java
@@ -0,0 +1,63 @@
+package com.algaworks.algashop.product.catalog.presentation;
+
+import com.algaworks.algashop.product.catalog.application.PageModel;
+import com.algaworks.algashop.product.catalog.application.category.management.CategoryInput;
+import com.algaworks.algashop.product.catalog.application.category.management.CategoryManagementService;
+import com.algaworks.algashop.product.catalog.application.category.query.CategoryDetailOutput;
+import com.algaworks.algashop.product.catalog.application.category.query.CategoryQueryService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/categories")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryQueryService categoryQueryService;
+    private final CategoryManagementService categoryManagementService;
+
+    @GetMapping
+    public PageModel<CategoryDetailOutput> filter(@RequestParam(defaultValue = "0") Integer page,
+                                                  @RequestParam(defaultValue = "10") Integer size) {
+        return categoryQueryService.filter(size,page);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CategoryDetailOutput create(@RequestBody @Valid CategoryInput input) {
+        UUID categoryId = categoryManagementService.create(input);
+        return categoryQueryService.findById(categoryId);
+    }
+
+    @GetMapping("/{categoryId}")
+    public CategoryDetailOutput findById(@PathVariable UUID categoryId) {
+        return categoryQueryService.findById(categoryId);
+    }
+
+    @PutMapping("/{categoryId}")
+    public CategoryDetailOutput update(
+            @PathVariable UUID categoryId,
+            @RequestBody @Valid CategoryInput input) {
+        categoryManagementService.update(categoryId, input);
+        return categoryQueryService.findById(categoryId);
+    }
+
+    @DeleteMapping("/{categoryId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void disable(@PathVariable UUID categoryId) {
+        categoryManagementService.disable(categoryId);
+    }
+}
